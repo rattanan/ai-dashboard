137a20b00b1991f621eab42fc12b790c545dc332
Rattanan Nantiyakul <rattanan@gmail.com>
2026-07-23 19:44:13 +0700

    oracle datetiem

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  serverExternalPackages: ["mysql2", "xlsx", "@node-rs/argon2"],
+  serverExternalPackages: ["mysql2", "oracledb", "xlsx", "@node-rs/argon2"],
   allowedDevOrigins: ["127.0.0.1"],
 };
 
